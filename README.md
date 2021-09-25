c6d30dd063a81c1c9482068893d6a846531d9567
Vins 2106 <69247128+Vins2106@users.noreply.github.com>
2021-09-25 18:11:13 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,18 @@
+![screenshot](./img/ss1.png)
+![screenshot](./img/ss2.png)
+![screenshot](./img/ss3.png)
+
+# Free Discord Bot Website Template
+### Today i make a `Discord Bot Website Template` for free (landing page only).
+
+# Installation
+### nodejs are not required for this project.
+#### 1. clone this project to your pc/laptop
+#### 2. open index.html on browser, tada!!! you can change that all on website :)
+#### 2. IMPORTANT: star this repo for more project
+
+# Note: if this repo reach 100 stars, i will make full page
+# But you can buy the full page if you doesnt want to waiting
+# DM me on instagram: @Vins2106
+
+# Make by Vins 2106 with html, css, js 
---
+++ js/script.js
@@ -0,0 +1,36 @@
+console.log("make by Vins 2106 - source code available on github (Vins2106)");
+
+setInterval(() => {
+	console.log("make by Vins 2106 - source code available on github (Vins2106)");
+}, 10000)
+
+let nt = document.getElementById("toggler");
+let nb = document.getElementById("navs");
+let ld = document.getElementById("loading-box")
+let body = document.querySelector("body");
+let isnt = false;
+
+function toggleNavbar(e) {
+
+	if (isnt) {
+		e.classList.remove("fa-times");
+		e.classList.add("fa-bars");
+		nb.classList.remove("on")
+		body.classList.remove("on")
+		isnt = false;
+	} else if (!isnt) {
+		e.classList.add("fa-times");
+		e.classList.remove("fa-bars");
+		nb.classList.add("on")
+		body.classList.add("on")
+		isnt = true;
+	}
+
+}
+
+window.onload = () => {
+	setTimeout(() => {
+		ld.classList.add("off");
+		body.classList.remove("on")
+	}, 1510);
+}
